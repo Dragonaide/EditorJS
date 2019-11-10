a12e1301a50bc9f3adc14e0d0910df26ada66882
Dragonaide <55767582+Dragonaide@users.noreply.github.com>
2019-11-10 14:55:35 -0500

    Update MainMenu.js

--- Components/MainMenu.js
+++ Components/MainMenu.js
@@ -2,4 +2,7 @@ export function MainMenu() {
   var navigation = document.createElement("nav");
   navigation.setAttribute("class", "main-menu");
   document.getElementById("editor").appendChild(navigation);
+  var navigationContent = document.createElement("ul");
+  navigationContent.setAttribute("class", "main-menu content");
+  navigation.appendChild(navigationContent);
 }
