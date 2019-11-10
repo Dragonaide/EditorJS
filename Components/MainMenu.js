a7ade383d67ecfa0f6a999e522fe2acc0bf8e390
Dragonaide <55767582+Dragonaide@users.noreply.github.com>
2019-11-10 14:50:01 -0500

    Create MainMenu.js

---
+++ Components/MainMenu.js
@@ -0,0 +1,5 @@
+export function MainMenu() {
+  var navigation = document.createElement("nav");
+  navigation.setAttribute("class", "main-menu");
+  document.getElementById("editor").appendChild(navigation);
+}
